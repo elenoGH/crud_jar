ba95a318c2e825e956f6c8f23fe0196e5f88736e
ELENO <goeleno@gmail.com>
2019-07-03 16:15:56 -0500

    se agregan a maven pom file las dependencias de spring(algunas) y se agrega la clace principal

---
+++ src/main/java/com/jade/crud_jar/Main.java
@@ -0,0 +1,12 @@
+
+package com.jade.crud_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
